cc814a7fc8dc31e02e63f5eba4e31d05aeb62213
meetvachheta17 <meetvachheta1@gmail.com>
2026-04-12 18:27:49 +0530

    Initial project upload

---
+++ Frontend/src/app.routes.jsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router-dom";
+import Login from "./Features/auth/pages/Login.jsx";
+import Register from "./Features/auth/pages/Register.jsx";
+import Home from "./Features/interview/pages/Home.jsx";
+import Protected from "./Features/auth/componets/Protected.jsx";
+
+export const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />,  
+  },
+  {
+    path: "/register",   
+    element: <Register />,
+  },
+  {
+    path: "/",
+    element: <Protected><Home /></Protected>
+  },
+  {
+    path:'"/interview/:interviewId',
+    element:<Protected><Home /></Protected>
+  }
+  
+])
+
