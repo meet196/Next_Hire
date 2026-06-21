b64331487f3ae89376a232290a2f1bdf8832406d
meetvachheta17 <meetvachheta1@gmail.com>
2026-06-21 19:30:00 +0530

    fix:debug cookies

--- Backend/src/middleware/auth.middleware.js
+++ Backend/src/middleware/auth.middleware.js
@@ -2,7 +2,6 @@ const jwt = require("jsonwebtoken")
 const tokenBlacklistModel = require("../models/blacklist.model")
 
 async function authUser(req, res, next) {
-    
     console.log("COOKIES RECEIVED:", req.cookies)
     const token = req.cookies.token
     if (!token) {
@@ -11,6 +10,7 @@ async function authUser(req, res, next) {
         })
     }
     const isTokenBlacklisted = await tokenBlacklistModel.findOne({token})
+    console.log("IS BLACKLISTED:", isTokenBlacklisted)
     if (isTokenBlacklisted) {
         return res.status(401).json({
             message: "token is invalid"
@@ -18,9 +18,11 @@ async function authUser(req, res, next) {
     }
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        console.log("DECODED SUCCESSFULLY:", decoded)
         req.user = decoded
         next()
     } catch (err) {
+        console.log("JWT VERIFY ERROR:", err.message)
         return res.status(401).json({
             message: "Invalid token."
         })
