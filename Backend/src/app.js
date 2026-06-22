5e6faf045c6854ca7d7b19a90dd0153588399b64
meetvachheta17 <meetvachheta1@gmail.com>
2026-06-22 14:28:43 +0530

    fix:some issue solved

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: ["http://localhost:5173", "https://nexthire-rouge.vercel.app"],
+    origin: ["http://localhost:5173", "https://next-hire-inky.vercel.app"],
     credentials: true
 }))
 
