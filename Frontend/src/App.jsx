710a4eeeb82b2bc20375966bbdc3b72f11d5d68e
meetvachheta17 <meetvachheta1@gmail.com>
2026-06-22 13:50:35 +0530

    fix:toster issue fixes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./app.routes.jsx";   
 import {AuthProvider} from "./Features/auth/auth.context.jsx";
 import { InterviewProvider } from "./Features/interview/interview.context.jsx";
+import { Toaster } from "react-hot-toast";
 
 function App() {
     return(
